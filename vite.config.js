b3b21c16095f97b4e569079625d3750706df627d
andresignaciodeagustini <andresignaciodeagustini@gmail.com>
2025-04-03 10:31:15 +0200

    actualizacionabril

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(''),  // URL vacía para usar rutas relativas
-  },
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8080',
+        target: mode === 'development' 
+          ? 'http://127.0.0.1:8080'
+          : 'https://expert-consultation-bot-back-ab9540834110.herokuapp.com',
         changeOrigin: true,
         secure: false
       }
     }
   }
-})
+}))
